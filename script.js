f1f818bdb75e5ad840bf3a872e735cd86b751154
J0rps <139977900+J0rps@users.noreply.github.com>
2024-04-17 15:17:50 +0000

    update

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 console.log('hi');
 
-let heading = document.querySelector("h1");
+let button = document.querySelector(".speaker");
+let screen = document.querySelector(".screen");
+let tvscreen = document.querySelector(".tvscreen");
+let tvname = document.querySelector(".tvname");
 
-heading.addEventListener("click", function(){
-    heading.style.color = 'green';
-    heading.innerText = 'Ready to Code';
+
+button.addEventListener("click", function(){
+
+  let page = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  let tvshow = Math.floor(Math.random() * (19 - 0 + 0)) + 0;
+  
+  let url = "https://www.episodate.com/api/most-popular?page=" + page
+   fetch(url)
+    .then(function(response){
+      console.log(response)
+      return response.json();
+    })
+  .then(data => {
+    tvImage = data.tv_shows[`${tvshow}`].image_thumbnail_path;
+    tvName = data.tv_shows[`${tvshow}`].name;
+    console.log(data.tv_shows[`${tvshow}`].name)
+    console.log(tvImage)
+    
+    tvscreen.src = tvImage;
+    tvname.innerHTML = tvName
+  })
+    .catch(error => console.error(error));
+    
 });
