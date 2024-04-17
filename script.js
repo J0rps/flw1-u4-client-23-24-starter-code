ae629d33053bb67161d86497f06cb95e7dda690f
J0rps <139977900+J0rps@users.noreply.github.com>
2024-04-17 22:10:53 +0000

    Update

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-console.log('hi');
 
+// assign variable from html to js
 let button = document.querySelector(".speaker");
 let screen = document.querySelector(".screen");
 let tvscreen = document.querySelector(".tvscreen");
 let tvname = document.querySelector(".tvname");
-
-
+let tvnetwork = document.querySelector(".tvnetwork");
+let tvstatus = document.querySelector(".tvstatus");
+//when click on tv button
 button.addEventListener("click", function(){
 
-  let page = Math.floor(Math.random() * (200 - 1 + 1)) + 1;
+  // generate number for page and tv show in the api
+  let page = Math.floor(Math.random() * (200 - 0 + 0)) + 0;
   let tvshow = Math.floor(Math.random() * (19 - 0 + 0)) + 0;
   
   let url = "https://www.episodate.com/api/most-popular?page=" + page
    fetch(url)
     .then(function(response){
-      console.log(response)
       return response.json();
+      // finds a page with random number
     })
   .then(data => {
+    // goes through api to the name and thumbnail and assign it to variable with random number.
     tvImage = data.tv_shows[`${tvshow}`].image_thumbnail_path;
     tvName = data.tv_shows[`${tvshow}`].name;
+    tvNetwork = data.tv_shows[`${tvshow}`].network;
+    tvStatus = data.tv_shows[`${tvshow}`].status;
+    
     console.log(data.tv_shows[`${tvshow}`].name)
     console.log(tvImage)
     
     tvscreen.src = tvImage;
-    tvname.innerHTML = tvName
+    tvname.innerHTML = tvName;
+    tvnetwork.innerHTML = "Network: " + tvNetwork;
+    tvstatus.innerHTML = "Status: " + tvStatus;
   })
-    .catch(error => console.error(error));
+    
     
 });
